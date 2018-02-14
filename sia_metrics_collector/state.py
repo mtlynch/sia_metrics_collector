6236f7ae9047d9786024dcefa7de34b67c502579
Michael Lynch <git@mtlynch.io>
2018-02-14 11:17:54 -0500

    Reimplementing SiaState as a recordtype

--- sia_metrics_collector/state.py
+++ sia_metrics_collector/state.py
@@ -1,3 +1,4 @@
+import recordtype
 import datetime
 import json
 import logging
@@ -12,118 +13,51 @@ def make_builder():
     return Builder(pysia.Sia(), datetime.datetime.utcnow)
 
 
-class SiaState(object):
-    """Represents a set of Sia metrics at a moment in time.
+"""Represents a set of Sia metrics at a moment in time.
 
-    Note that the timestamp is *roughly* the time these metrics were collected.
-    It's accurate to within a few seconds, but shouldn't be trusted beyond that,
-    as different metrics come from different API calls made sequentially, each
-    with a small amount of latency.
-    """
+Note that the timestamp is *roughly* the time these metrics were collected.
+It's accurate to within a few seconds, but shouldn't be trusted beyond that,
+as different metrics come from different API calls made sequentially, each
+with a small amount of latency.
 
-    def __init__(self,
-                 timestamp=None,
-                 contract_count=None,
-                 total_contract_size=None,
-                 file_count=None,
-                 total_file_bytes=None,
-                 uploads_in_progress_count=None,
-                 uploaded_bytes=None,
-                 total_contract_spending=None,
-                 contract_fee_spending=None,
-                 storage_spending=None,
-                 upload_spending=None,
-                 download_spending=None,
-                 remaining_renter_funds=None,
-                 wallet_siacoin_balance=None,
-                 api_latency=None):
-        """Creates a new SiaState instance.
-
-        Args:
-            timestamp: Time at which the metrics were collected.
-            contract_count: Number of active Sia contracts.
-            total_contract_size: Total size of all Sia contracts (this should be
-                equal to uploaded_bytes, but the data come from different
-                sources).
-            file_count: Number of files known to Sia (either partially or
-                fully uploaded).
-            total_file_bytes: Total size of all files files known to Sia. Not
-                all bytes have necessarily been uploaded to Sia yet.
-            uploads_in_progress_count: Number of uploads currently in progress.
-            uploaded_bytes: Total number of bytes that have been uploaded
-                across all files.
-            total_contract_spending: Total amount of money (in hastings) spent
-                on storage contracts.
-            contract_fee_spending: Total amount of money (in hastings) spent on
-                contract fees.
-            storage_spending: Total amount of money (in hastings) spent on
-                storage.
-            upload_spending: Total amount of money (in hastings) spent on upload
-                bandwidth.
-            download_spending: Total amount of money (in hastings) spent on
-                download bandwidth.
-            wallet_siacoin_balance: Current wallet balance of Siacoins (in
-                hastings).
-            api_latency: Time (in milliseconds) it took for Sia to respond to
-                all API calls.
-        """
-        self.timestamp = timestamp
-        self.contract_count = contract_count
-        self.total_contract_size = total_contract_size
-        self.file_count = file_count
-        self.total_file_bytes = total_file_bytes
-        self.uploads_in_progress_count = uploads_in_progress_count
-        self.uploaded_bytes = uploaded_bytes
-        self.total_contract_spending = total_contract_spending
-        self.contract_fee_spending = contract_fee_spending
-        self.storage_spending = storage_spending
-        self.upload_spending = upload_spending
-        self.download_spending = download_spending
-        self.remaining_renter_funds = remaining_renter_funds
-        self.wallet_siacoin_balance = wallet_siacoin_balance
-        self.api_latency = api_latency
-
-    def __eq__(self, other):
-        return (
-            (self.timestamp == other.timestamp) and
-            (self.contract_count == other.contract_count) and
-            (self.total_contract_size == other.total_contract_size) and
-            (self.file_count == other.file_count) and
-            (self.total_file_bytes == other.total_file_bytes) and
-            (self.uploads_in_progress_count == other.uploads_in_progress_count)
-            and (self.uploaded_bytes == other.uploaded_bytes) and
-            (self.total_contract_spending == other.total_contract_spending) and
-            (self.contract_fee_spending == other.contract_fee_spending) and
-            (self.storage_spending == other.storage_spending) and
-            (self.upload_spending == other.upload_spending) and
-            (self.download_spending == other.download_spending) and
-            (self.remaining_renter_funds == other.remaining_renter_funds) and
-            (self.wallet_siacoin_balance == other.wallet_siacoin_balance) and
-            (self.api_latency == other.api_latency))
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __repr__(self):
-        as_dict = {
-            'contract_count': self.contract_count,
-            'total_contract_size': self.total_contract_size,
-            'file_count': self.file_count,
-            'total_file_bytes': self.total_file_bytes,
-            'uploads_in_progress_count': self.uploads_in_progress_count,
-            'uploaded_bytes': self.uploaded_bytes,
-            'total_contract_spending': self.total_contract_spending,
-            'contract_fee_spending': self.contract_fee_spending,
-            'storage_spending': self.storage_spending,
-            'upload_spending': self.upload_spending,
-            'download_spending': self.download_spending,
-            'remaining_renter_funds': self.remaining_renter_funds,
-            'wallet_siacoin_balance': self.wallet_siacoin_balance,
-            'api_latency': self.api_latency,
-        }
-        if self.timestamp:
-            as_dict['timestamp'] = self.timestamp.strftime('%Y-%m-%dT%H:%M:%S')
-        return json.dumps(as_dict)
+Fields:
+    timestamp: Time at which the metrics were collected.
+    contract_count: Number of active Sia contracts.
+    total_contract_size: Total size of all Sia contracts (this should be
+        equal to uploaded_bytes, but the data come from different
+        sources).
+    file_count: Number of files known to Sia (either partially or
+        fully uploaded).
+    total_file_bytes: Total size of all files files known to Sia. Not
+        all bytes have necessarily been uploaded to Sia yet.
+    uploads_in_progress_count: Number of uploads currently in progress.
+    uploaded_bytes: Total number of bytes that have been uploaded
+        across all files.
+    total_contract_spending: Total amount of money (in hastings) spent
+        on storage contracts.
+    contract_fee_spending: Total amount of money (in hastings) spent on
+        contract fees.
+    storage_spending: Total amount of money (in hastings) spent on
+        storage.
+    upload_spending: Total amount of money (in hastings) spent on upload
+        bandwidth.
+    download_spending: Total amount of money (in hastings) spent on
+        download bandwidth.
+    wallet_siacoin_balance: Current wallet balance of Siacoins (in
+        hastings).
+    api_latency: Time (in milliseconds) it took for Sia to respond to
+        all API calls.
+"""
+SiaState = recordtype.recordtype(
+    'SiaState', [
+        'timestamp', 'contract_count', 'total_contract_size', 'file_count',
+        'total_file_bytes', 'uploads_in_progress_count', 'uploaded_bytes',
+        'total_contract_spending', 'contract_fee_spending', 'storage_spending',
+        'upload_spending', 'download_spending', 'remaining_renter_funds',
+        'wallet_siacoin_balance', 'api_latency'
+    ],
+    default=None)
+SiaState.as_dict = SiaState._asdict
 
 
 class Builder(object):
